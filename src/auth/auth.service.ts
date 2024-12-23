d8f074c69754897d2336e9411361eafc11375657
ChrisJulLimantoro <christopherlimantoro@gmail.com>
2024-12-23 13:35:13 +0700

    feat: done authentication request

--- src/auth/auth.service.ts
+++ src/auth/auth.service.ts
@@ -3,11 +3,16 @@ import { LoginRequest } from './dto/login-request.dto';
 import { UserRepository } from 'src/repositories/user.repository';
 import { CustomResponse } from 'src/exception/dto/custom-response.dto';
 import * as bcrypt from 'bcrypt';
+import { ValidationService } from 'src/validation/validation.service';
 
 @Injectable()
 export class AuthService {
-  constructor(private repository: UserRepository) {}
+  constructor(
+    private repository: UserRepository,
+    private readonly validation: ValidationService,
+  ) {}
   async login(data: LoginRequest) {
+    this.validation.validate(data, LoginRequest.schema());
     const user = await this.repository.authenticateEmail(data.email);
     if (!user) {
       return CustomResponse.error('User not found', null, 404);
@@ -17,6 +22,11 @@ export class AuthService {
     if (!passwordMatch) {
       return CustomResponse.error('Invalid password', null, 400);
     }
-    return CustomResponse.success('Login successful', user, 200);
+    //TODO: IMPLEMENT MORE USER DATA TO BE RETURNED
+    const userData = {
+      id: user.id,
+      email: user.email,
+    };
+    return CustomResponse.success('Login successful', userData, 200);
   }
 }
