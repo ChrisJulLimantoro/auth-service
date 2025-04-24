2d7d2701fc91cdb68f79813a9f8d10ee3b9be34f
ChrisJulLimantoro <christopherlimantoro@gmail.com>
2025-04-24 12:12:59 +0700

    feat: add seeding

--- prisma/seeder/user.seeder.ts
+++ prisma/seeder/user.seeder.ts
@@ -1,34 +1,37 @@
-import { PrismaClient } from '@prisma/client';
-import * as bcrypt from 'bcrypt';
+// import { PrismaClient } from '@prisma/client';
+// import * as bcrypt from 'bcrypt';
 
-const prisma = new PrismaClient();
+// const prisma = new PrismaClient();
 
-async function main() {
-  // Create default companies
-  const data = await prisma.user.findFirst({
-    where: {
-      email: 'christian@gmail.com',
-    },
-  });
-  if (data) {
-    console.log('User already exists, skipping creation');
-    return;
-  }
-  const user1 = await prisma.user.create({
-    data: {
-      email: 'christian@gmail.com',
-      password: await bcrypt.hashSync('password', 10),
-    },
-  });
+// async function main() {
+//   // Create default companies
+//   const data = await prisma.user.findFirst({
+//     where: {
+//       email: 'christian@gmail.com',
+//     },
+//   });
+//   if (data) {
+//     console.log('User already exists, skipping creation');
+//     return;
+//   }
+//   const user1 = await prisma.user.create({
+//     data: {
+//       email: 'christian@gmail.com',
+//       password: await bcrypt.hashSync('password', 10),
+//       is_owner: true,
+//     },
+//   });
 
-  console.log('Seed data created:', { user1 });
-}
+//   // Send this to RabbitMQ for the other services to consume
 
-main()
-  .catch((e) => {
-    console.error(e);
-    process.exit(1);
-  })
-  .finally(async () => {
-    await prisma.$disconnect();
-  });
+//   console.log('Seed data created:', { user1 });
+// }
+
+// main()
+//   .catch((e) => {
+//     console.error(e);
+//     process.exit(1);
+//   })
+//   .finally(async () => {
+//     await prisma.$disconnect();
+//   });
